425121c08ac7127afbf94cabb226c9f6e2da00fe
Husky Lover <ssolivares0@gmail.com>
2024-07-06 11:14:13 -0400

    Created User model

---
+++ models/User.js
@@ -0,0 +1,32 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class User extends Model { }
+
+User.init(
+  {
+    id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "user",
+  }
+);
+
+//export the User model
+module.exports = User;
